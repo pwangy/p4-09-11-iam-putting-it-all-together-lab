3d962d533b29dbbd4e2d49e7afb94c831a86ba95
pwangy <blipsandclicks@gmail.com>
2024-04-10 08:57:19 -0700

    models

--- server/models.py
+++ server/models.py
@@ -3,11 +3,8 @@ from sqlalchemy_serializer import SerializerMixin
 from config import db, bcrypt
 
 
-# incorporate bcrypt to create a secure password. Attempts to access the password_hash should be met with an AttributeError.
-# constrain the user's username to be present and unique (no two users can have the same username).
-# have many recipes.
 class User(db.Model, SerializerMixin):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False, unique=True)
@@ -15,26 +12,36 @@ class User(db.Model, SerializerMixin):
     image_url = db.Column(db.String)
     bio = db.Column(db.String)
 
+    recipes = db.relationship("Recipe", back_populates="user")
+    serialize_rules = ("-recipes.user", "-_password_hash")
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes may not be viewed.")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
+        self._password_hash = password_hash.decode("utf-8")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
+
     def __repr(self):
-        return f'<User {self.username}>'
+        return f"<User {self.username}, ID: {self.id} >"
 
-
-# a recipe belongs to a user.
-# id that is an integer type and a primary key.
-# title that is a String type.
-# instructions that is a String type.
-# minutes_to_complete that is an Integer type.
-
-# constrain the title to be present.
-# constrain the instructions to be present and at least 50 characters long.
 
 class Recipe(db.Model, SerializerMixin):
-    __tablename__ = 'recipes'
+    __tablename__ = "recipes"
+    __table_arts__ = (db.CheckConstraint("length(instructions) >= 50"),)
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     instructions = db.Column(db.String, nullable=False)
     minutes_to_complete = db.Column(db.Integer)
+    user_id = db.Column(db.Integer(), db.ForeignKey("users.id"))
+
+    user = db.relationship("User", back_populates="recipes")
 
     def __repr__(self):
-        return f'<Recipe {self.id}: {self.title}>'
+        return f"<Recipe {self.id}: {self.title}: {self.instructions}>"
